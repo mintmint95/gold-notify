8ec0137a8d6a09c721e46109b18827b1548a601f
mintmint95 <khwankamol.mint@gmail.com>
2021-10-14 00:55:55 +0700

    fix: edit run command

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 
 const config = {
   schedule: '* * * * *',
