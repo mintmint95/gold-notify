4a88004b2c4c517368e9ed6eadcd2a1d7cd42109
mintmint95 <khwankamol.mint@gmail.com>
2021-10-13 16:27:55 +0700

    feat: create cron schedule

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cron = require('node-cron')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,13 @@ app.get('/', (req, res) => {
 app.post('/webhook', (req, res) => {
   res.send('test webhook')
 })
+
+cron.schedule('* * * * *', () => {
+   console.log('Running a task every minute at Asia/Bangkok');
+ }, {
+   scheduled: true,
+   timezone: "Asia/Bangkok"
+ });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
