3c676a51af375c1800fdabebe069f9237619c05b
infernalslam <infernal-slam@hotmail.com>
2021-10-13 16:02:08 +0700

    feat: implement webhook for line OA

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/webhook', (req, res) => {
+  res.send('test webhook')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
