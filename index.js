c55ffe673975f5e89c429356c8702ec2258d07ba
mintmint95 <khwankamol.mint@gmail.com>
2021-10-13 16:38:20 +0700

    feat: create cron schedule to get gold price

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cron = require('node-cron')
+const axios = require('axios')
+
 const app = express()
 const port = 3000
 
@@ -12,12 +14,19 @@ app.post('/webhook', (req, res) => {
 })
 
 cron.schedule('* * * * *', () => {
-   console.log('Running a task every minute at Asia/Bangkok');
+   //console.log('Running a task every minute at Asia/Bangkok');
+   axios.get('https://thai-gold-api.herokuapp.com/latest')
+   .then(function (response) {
+    // handle success
+      console.log(response.data);
+    })
  }, {
    scheduled: true,
-   timezone: "Asia/Bangkok"
+   timezone: 'Asia/Bangkok'
  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
